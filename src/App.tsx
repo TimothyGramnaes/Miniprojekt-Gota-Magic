7063d9f119fba64d19ea670a85d3b636cb74a633
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-03-10 13:50:12 +0100

    !

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello wörld
+          Hello wörld!
         </a>
       </header>
     </div>
