cdae241dda945f8ebc86b059b692ff742f2a9aab
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-03-11 09:44:58 +0100

    Component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 
 import './App.css';
+import Layout from './Components/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello wörld!
-        </a>
-      </header>
-    </div>
+  
+      <Layout />
+    
   );
 }
 
