ec0f936475dfeb4b37b5640d80d624163db1300a
Oliver Nygren <oliver.nygren@icloud.com>
2021-03-11 09:52:00 +0100

    added main.css file and theme provider with a theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FF7A2F', //Orange
+    },
+    secondary: {
+      main: '#000000', //Black
+      light: '#EDEDED', //Light Grey
+      dark: '#393939' //Dark Grey
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello wörld!
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>  
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello wörld!
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
