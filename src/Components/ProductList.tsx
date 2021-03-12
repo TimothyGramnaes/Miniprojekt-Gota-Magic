d06e0cc1dec3de5962c08de10981ee5ee0203ba3
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-03-12 11:45:30 +0100

    added pagination

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,8 +1,13 @@
 import { CSSProperties } from '@material-ui/styles'
 import { products } from '../DB/Products'
 import ProductListCard from './ProductListCard'
+import { useState } from 'react'
+import { Button } from '@material-ui/core'
 
 function ProductList() {
+    const [page, setPage] = useState(0)
+    const [pageItems, setPageItems] = useState(5)
+
     const listStyle: CSSProperties = {
         display: 'flex',
         flexWrap: 'wrap',
@@ -18,9 +23,40 @@ function ProductList() {
         )
     )
 
+    const paginationData=products.slice(page, pageItems).map(item => (
+        <p>Kortnummer: {item.id}/ </p>
+    ))
+
+    const decrease = () => {
+        const thisPage:number = page - 5
+        const thisPageItems:number = pageItems - 5
+        setPage(thisPage)
+        setPageItems(thisPageItems)
+    }
+
+    const increase = () => {
+        const thisPage:number = page + 5
+        const thisPageItems:number = pageItems + 5
+        setPage(thisPage)
+        setPageItems(thisPageItems)
+    }
+
     return(
         <div style={listStyle}>            
             {productData}
+            <h2>PAGINATION</h2>
+            <div className="products">
+            {paginationData}
+            </div>
+            <div className="productListBtn" style={{padding:'2rem 2rem'}}>
+                <Button onClick={decrease}>
+                    T
+                </Button>
+                <p>Sida 1</p>
+                <Button onClick={increase}>
+                    F
+                </Button>
+            </div>
         </div>
     )
 }
