7dc4508d7c6328fbd899e64fe6c4130da9122319
Olof-Wallgren <naturel.olof@hotmail.com>
2021-03-12 09:50:13 +0100

    finalFix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import Header from "./Components/headerComponent/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
