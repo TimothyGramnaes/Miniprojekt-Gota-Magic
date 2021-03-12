b01186cb4fd8f923e2c2983a78c48b26ce2091ea
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2021-03-12 14:26:50 +0100

    styled pagination

--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -2,7 +2,9 @@ import { CSSProperties } from '@material-ui/styles'
 import { products } from '../DB/Products'
 import ProductListCard from './ProductListCard'
 import { useState } from 'react'
-import { Button } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 function ProductList() {
     const [page, setPage] = useState(0)
@@ -14,10 +16,32 @@ function ProductList() {
 
     const listStyle: CSSProperties = {
         display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         flexWrap: 'wrap',
         maxWidth: '48rem',
         width: '100%',
         margin: '0 auto'
+    }
+
+    const productListBtnStyle: CSSProperties = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
+    }
+
+    const pagesIconStyle: CSSProperties = {
+        width: '5rem',
+        height: '5rem',
+        borderRadius: '500rem',
+        backgroundColor: '#393939',
+        color: '#FFFFFF',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '2rem 1rem'
+        
     }
 
     const productData=products.slice(page, pageItems).map(product => (
@@ -70,17 +94,21 @@ function ProductList() {
     }
 
     return(
-        <div style={listStyle}>            
-            {productData}
-            
-            <div className="productListBtn" style={{padding:'2rem 2rem'}}>
-                <Button onClick={decrease}>
-                    T
-                </Button>
-                <p>{pageNumber}</p>
-                <Button onClick={increase}>
-                    F
-                </Button>
+        <div className="productListContainer">
+            <div style={listStyle}>            
+                {productData}
+                
+                <div className="productListBtn" style={productListBtnStyle}>
+                    <IconButton onClick={decrease}>
+                        <ArrowBackIcon />
+                    </IconButton>
+                    <div className="pageCircle" style={pagesIconStyle}>
+                        <p >Sida {pageNumber}</p>
+                    </div>
+                    <IconButton onClick={increase}>
+                        <ArrowForwardIcon />
+                    </IconButton>
+                </div>
             </div>
         </div>
     )
