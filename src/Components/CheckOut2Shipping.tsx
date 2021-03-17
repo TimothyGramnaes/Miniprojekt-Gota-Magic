e0d6cb113d3f024c906e99f80d9509eddc4dedb5
Oliver Nygren <oliver.nygren@icloud.com>
2021-03-17 15:38:32 +0100

    added hook, recongnize active btn + change

--- src/Components/CheckOut2Shipping.tsx
+++ src/Components/CheckOut2Shipping.tsx
@@ -1,11 +1,29 @@
 import TextField from '@material-ui/core/TextField';
 import '../css/checkOut2Shipping.css'
 import '../main.css'
-import { Button, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import { Button, FormControl, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
 import BreadCrumbs from "./BreadCrumbs";
-import React from 'react';
+import React, { useState } from 'react';
+import { ShippingMethod, shippingMethods } from '../DB/ShippingMethods';
 
 function CheckOut2Shipping() {
+
+  const [value, setValue] = useState('')
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+    console.log('handleRadioChange says: ' + value)
+  }
+
+  value === 'postnord' ? console.log(value) :
+  value === 'earlybird' ? console.log(value) :
+  value === 'instabox' ? console.log(value) :
+  value === 'brevduva' ? console.log(value) :
+  console.log('current value is ' + value)
+
+  // Shipping methods array
+  const [shippingObject, setShippingObject] = useState<ShippingMethod[]>([])
+  
   return (
     <div className="background">
       <div className="grey-card">
@@ -21,15 +39,17 @@ function CheckOut2Shipping() {
             </div>
             <div className="checkout-form">
               <form className="flex column">
-                <RadioGroup>
-                  <FormControlLabel className="input-field" value="postnord" control={<Radio />} label="Postnord - 9 kr (14-21 arbetsdagar)" />
-                  <FormControlLabel className="input-field" value="earlybird" control={<Radio />} label="EarlyBird - 29 kr (1-3 arbetsdagar)" />
-                  <FormControlLabel className="input-field" value="instabox" control={<Radio />} label="Instabox - 19 kr (1-3 arbetsdagar)" />
-                  <FormControlLabel className="input-field" value="brevduva" control={<Radio />} label="Brevduva  - Gratis (1 arbetsdag)" />
-                  <FormControlLabel className="input-field" value="magicshipping" control={<Radio />} label="Magic Shipping - 199 kr (1 timme)" />
-                </RadioGroup>
+                <FormControl>
+                  <RadioGroup onChange={handleRadioChange}>
+                    <FormControlLabel id="1" className="input-field" value="postnord" control={<Radio />} label={shippingMethods[0].name} />
+                    <FormControlLabel id="2" className="input-field" value="earlybird" control={<Radio />} label="EarlyBird - 29 kr (1-3 arbetsdagar)" />
+                    <FormControlLabel id="3" className="input-field" value="instabox" control={<Radio />} label="Instabox - 19 kr (1-3 arbetsdagar)" />
+                    <FormControlLabel id="4" className="input-field" value="brevduva" control={<Radio />} label="Brevduva  - Gratis (1 arbetsdag)" />
+                    <FormControlLabel id="5" className="input-field" value="magicshipping" control={<Radio />} label="Magic Shipping - 199 kr (1 timme)" />
+                  </RadioGroup>
+                </FormControl>
               </form>
-                <Button variant="contained" color="primary" className="move-fwd-btn">Till Betalning</Button>
+              <Button variant="contained" color="primary" className="move-fwd-btn">Till Betalning</Button>
             </div>
           </div>
 
