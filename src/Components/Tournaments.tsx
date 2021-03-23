8472d87e036a30f6058f4b787266e3ccc6b08a90
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-03-23 16:38:14 +0100

    Turneringskomponenten avklarad

--- src/Components/Tournaments.tsx
+++ src/Components/Tournaments.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from '@material-ui/styles';
 import React from 'react'
 import '../main.css';
+import '../css/tournaments.css';
 import { Link } from 'react-router-dom';
 import { IconButton, Grid, TextField, Button, makeStyles } from '@material-ui/core'
 
@@ -16,27 +17,67 @@ function Tournaments() {
         style={landingContainer}>
 
             {/* <div className="flex centered column" style={infoLandingContainer}> */}
-            <Grid item xs={12} md={10} style={infoLandingContainer}>
+            <Grid container xs={12} md={10} style={infoLandingContainer}>
   
-               <img style={infoLandingContainerImage} src="./assets/imgs/play/bear2.jpg" alt="planeswalkers" />                   
-                <div className="flex centered column" style={textLandingContainer}>
-                <h2 className="padding2rem">Turneringar</h2>
-                <span></span>
-                    <p className="paragraphs">
-                        Det spelas regelbundna turneringar varje lördag på varierade platser i Göteborgsområdet
+               <img style={infoLandingContainerImage} src="./assets/imgs/play/bear2.jpg" alt="glatt-turnerings-sallskap" />                   
+                <Grid container className="flex" style={titleHeader}>
+                    <h2>
+                        Turneringar                       
+                    </h2>
+                </Grid>
+                <Grid container className="flex" style={textLandingContainer}>
+                    <Grid container className="tour-kort">
+
+                    <Grid className="tour-kort-item" item xs={6} md={8}>
+                    <h2>Lördagsmagi</h2>
+                    <p className="kort-p">
+                        Varje lördag spelas det turnering i Legacy/Modern på 
+                        lite olika lokaler runt om i Göteborgsområdet.
+                        Första pris är att få skriva sitt namn i den anrika
+                        propeller-kepsen, man erhåller även rätten att ha den
+                        i sin ägo fram till nästa turneringstillfälle.
+                        <br/>
+                        <br/>
+                        Nu under corona-tider är vi dessvärre tvugna att max
+                        vara 8 personer under våra spelträffar. kontakta oss här
+                        för att ställa dig i kö så kontaktar vi dig om plats finnes.
+                        Det är dock många som står i kö så räkna med att det kan 
+                        ta ett tag innan ni kan få möjlighet att vara med.
                     </p>
-                <h3 className="padding2rem">
-                Underrubrik igen
-                 </h3>
-                    <p className="paragraphs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo esse odio minima ipsa hic, nobis architecto amet? Porro neque in atque soluta eum officiis magni doloremque ipsum? Nihil, explicabo voluptates!</p>
-        <div className="padding2rem">
-            <Link className="link-style" to="/ProductList">
-            <Button variant="contained" color="primary">Våra Produkter</Button>
-            </Link>
-        </div>
-        </div>
-        {/* </div> */}
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <img className="person-bild" src="./assets/imgs/play/petrus.jpeg" alt="petrus" />
+                        <p className="pic-text">Petter Mölder, stolt vinnare av segerkepsen 2021-03-20</p>
+                    </Grid>
+                    </Grid>
+                    <div className="breaker"></div>
+                
+                    <Grid container className="tour-kort">
+                        <Grid item xs={6} md={4}>
+                            <img className="person-bild" src="./assets/imgs/play/eddie.png" alt="eddie" />
+                            <p className="pic-text">
+                                Edvin Boije, vinnare av Modbearn -2020
+                            </p>
+                        </Grid>
+                        <Grid className="tour-kort-item-right" item xs={6} md={8}>
+                            <h2>Bearvitational</h2>
+                            <p className="kort-p">
+                                Även kännt som Modbearn eller bara årets höjdpunkt.
+                                <br/>
+                                Formatet är Modern med en twist. Bannade kort är tillåtna
+                                men för varje bannat kort i leken måste man spela en björn. dvs. en
+                                2/2 för cmc 2.
+                                <br/>
+                                <br/>
+                                Lokal samt tillfälle för Bearvitational 2021 är för närvarande oklart 
+                                pga. pågående pandemi.
+                            </p>
+                        </Grid>
+
+                    </Grid>
+
+            </Grid>
+        
         </Grid>
     </Grid>
     
@@ -47,11 +88,24 @@ function Tournaments() {
 const landingContainer: CSSProperties = {
     backgroundImage: 'url(./assets/imgs/what-the-hex.png)',    
     height: '100%',
+    
+}
+
+const titleHeader: CSSProperties = {
+    fontSize: '2rem',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '1rem',
+    width: '100%',
+    backgroundImage: 'url(./assets/imgs/mtg)',
+    objectFit: 'cover',
+    color: 'white'
 }
 
 const infoLandingContainer: CSSProperties = {
-    width: '80vw',
+   
     maxWidth: '75rem',
+
     margin: '4rem 2rem 2rem 2rem',
     top: 0,
     bottom: 0,
@@ -65,6 +119,7 @@ const infoLandingContainerImage: CSSProperties = {
     objectFit: 'cover',
     top: 0,
     bottom: 0,
+    // border: '2px solid black'
 }
 
 const textLandingContainer: CSSProperties = {
