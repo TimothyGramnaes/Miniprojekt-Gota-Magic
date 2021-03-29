29bb0950987e938385f3785b64dea9bb66b47aff
Olof-Wallgren <naturel.olof@hotmail.com>
2021-03-29 15:50:07 +0200

    formTextField

---
+++ src/Components/admin/AdminForm.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+
+function AdminForm() {
+  return (
+    <form action="">
+      <div>
+        <TextField
+          label="Size"
+          id="standard-size-normal"
+          defaultValue="Normal"
+        />
+        <TextField
+          label="Size"
+          id="standard-size-normal"
+          defaultValue="Normal"
+        />
+        <TextField
+          label="Size"
+          id="standard-size-normal"
+          defaultValue="Normal"
+        />
+        <TextField
+          label="Size"
+          id="standard-size-normal"
+          defaultValue="Normal"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default AdminForm;
