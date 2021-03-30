2c651819a1484db50971fddfcda41804204ee228
Oliver Nygren <oliver.nygren@icloud.com>
2021-03-30 14:32:00 +0200

    settimeout

--- src/Components/OrderConfirmation.tsx
+++ src/Components/OrderConfirmation.tsx
@@ -24,10 +24,9 @@ function OrderConfirmation() {
           <h3>Detta är ett bevis på ditt köp. Spara denna orderbekräftelse!</h3>
         </div>
         <div className="user-info">
-          {/* <h5 className="order-info-text">
-            Din leverans beräknas anlända till **ADRESS** om ca **FRAKTSÄTT
-            LÄNGD**
-          </h5> */}
+          <h5 className="order-info-text">
+            Din leverans beräknas anlända till {user.deliveryaddress} om ca {shipping[0].deliveryTime}
+          </h5>
           <h3>Användaruppgifter</h3>
           <p>{user.name}</p>
           <p>{user.email}</p>
